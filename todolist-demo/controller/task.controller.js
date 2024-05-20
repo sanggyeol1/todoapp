6c8cd2054d92b30313f7d49cc6552a0dfdda7424
sanggyeol <sanggyeol1@gmail.com>
2024-05-20 16:55:32 +0900

    update

--- todolist-demo/controller/task.controller.js
+++ todolist-demo/controller/task.controller.js
@@ -1,37 +1,56 @@
 const Task = require("../model/Task")
+const { ObjectId } = require('mongodb');
 
-const taskController = { }
+const taskController = {}
 
-taskController.createTask = async (req, res)=>{
-    try{
+taskController.createTask = async (req, res) => {
+    try {
         const { task, isComplete } = req.body;
-        const newTask = new Task({task, isComplete})
+        const newTask = new Task({ task, isComplete })
         await newTask.save();
-        res.status(200).json({status : "ok", data : newTask})
-    }catch(err){
-        res.status(400).json({status:"fail", error : err})
+        res.status(200).json({ status: "ok", data: newTask })
+    } catch (err) {
+        res.status(400).json({ status: "fail", error: err })
     }
 }
 
-taskController.getTask = async (req, res) =>{
-    try{
+taskController.getTask = async (req, res) => {
+    try {
         const taskList = await Task.find({}).select("-__v")
-        res.status(200).json({status:"ok", data : taskList})
-    }catch(err){
-        res.status(400).json({status:"fail", error : err})
+        res.status(200).json({ status: "ok", data: taskList })
+    } catch (err) {
+        res.status(400).json({ status: "fail", error: err })
     }
 }
 
-taskController.deleteTask = async (req, res)=>{
-    try{
-        const { id } = req.body;
-        const newTask = new Task({ id })
-        
-        res.status(200).json({status : "ok", data : newTask})
-    }catch(err){
-        res.status(400).json({status:"fail", error : err})
+taskController.deleteTask = async (req, res) => {
+    try {
+        const taskId = new ObjectId(req.params.id)
+        const result = await Task.deleteOne({ _id: taskId });
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ status: "fail", error: "Task not found" });
+        }
+        res.status(200).json({ status: "ok", data: result })
+    } catch (err) {
+        res.status(400).json({ status: "fail", error: err })
+    }
+}
+
+taskController.updateTask = async (req, res) => {
+    try {
+        const taskId = new ObjectId(req.params.id)
+        const task = await Task.findOne({ _id: taskId })
+        const newStatus = !task.isComplete;
+
+        const result = await Task.updateOne({ _id: taskId }, { $set: { isComplete: newStatus } });
+        if (result.modifiedCount === 0) {
+            return res.status(404).json({ status: "fail", error: "Task not found" });
+        }
+        res.status(200).json({ status: "ok", data: result })
+    } catch (err) {
+        res.status(400).json({ status: "fail", error: err })
     }
 }
 
 
-module.exports= taskController
+module.exports = taskController
