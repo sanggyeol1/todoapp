0edb1197538624c75dcfa6afafe9d8c80fb23f31
sanggyeol <sanggyeol1@gmail.com>
2024-05-24 22:12:22 +0900

    Remove node_modules from tracking

---
+++ todolist-demo/controller/user.controller.js
@@ -0,0 +1,47 @@
+const User = require("../model/User")
+const bcrypt = require("bcrypt")
+const saltRounds = 10
+
+const userController = {}
+
+//회원가입
+userController.createUser = async (req, res) => {
+    try {
+        const { email, name, password } = req.body
+
+        const user = await User.findOne({ email })
+        if (user) {
+            throw new Error("이미 가입된 유저입니다.")
+        }
+
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hash = bcrypt.hashSync(password, salt);
+        const newUser = new User({ email, name, password: hash })
+        await newUser.save()
+
+
+        res.status(200).json({ status: "success" })
+    } catch (err) {
+        res.status(401).json({ status: "fail", error: err })
+    }
+}
+
+//로그인
+userController.loginWithEmail = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email },"-createdAt -updatedAt -__v")//필요한 정보만 프로젝션
+        if (user){
+            const isMath = bcrypt.compareSync(password, user.password) // bcrypt비교함수로 암호화된pw와 req.body.pw비교
+            if(isMath){//이메일 pw가 match하면 토큰 발행
+                const token = user.generateToken()
+                return res.status(200).json({status:"success", user, token})
+            }
+        }
+        throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.")
+    } catch (err) {
+        res.status(400).json({status:"fail",  err : err.message})
+    }
+}
+
+module.exports = userController
--- todolist-demo/routes/user.api.js
+++ todolist-demo/routes/user.api.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router();
+const userController = require("../controller/user.controller")
 
 //1. 회원가입 endpoint
-router.post("/", (req, res)=>{
-    res.send("create user controller will be here")
-})
+router.post("/", userController.createUser)
+//로그인
+router.post("/login", userController.loginWithEmail)
 
 module.exports = router;
